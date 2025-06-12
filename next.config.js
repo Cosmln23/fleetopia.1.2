dc33bf329cceeff551a45ac958264f8e6371d260
Cosmln23 <scinterim09@gmail.com>
2025-06-13 00:22:51 +0200

    build: add webpack alias for '@' to fix module resolution in production

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 const sentryConfig = {
